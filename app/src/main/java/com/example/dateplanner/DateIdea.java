fd5e16abb809ca01c4f8b25c709375025f259c41
chanf2824 <fwchan@ucsd.edu>
2021-02-23 11:34:52 -0800

    Changed error in gradient_background, cleaned up colors.sml, added home page, updated gradle. Pushing to master to fix the error in gradient_background

---
+++ app/src/main/java/com/example/dateplanner/DateIdea.java
@@ -0,0 +1,9 @@
+package com.example.dateplanner;
+
+/*
+This class will represent a date idea/post someone has
+It will include associated images, description, and metadata about it
+ */
+public class DateIdea {
+
+}
